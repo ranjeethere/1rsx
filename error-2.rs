e3cc83a1c705ab3eae77f7f8239d1093f7d9d294
FoGK <147843523+ranjeethere@users.noreply.github.com>
2024-05-22 23:49:18 +0530

    Create error-2.rs

---
+++ error-2.rs
@@ -0,0 +1,15 @@
+// Recoverable error
+
+use std::fs::File;
+
+fn main() {
+    let data_result = File::open("data.txt");
+
+    // using match for Result type
+    let data_file = match data_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the data file: {:?}", error),
+    };
+
+    println!("Data file", data_file);
+}
